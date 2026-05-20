a658b684a17f3797f972b7b53fb7b2e6985500e7
Robert Zacchia <robert.zacchia@students.uibk.ac.at>
2026-05-20 23:58:20 +0200

    added shape helpers, so we do not need sf dependencies in controller

---
+++ include/shapes/rectangle.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#include <algorithm>
+#include <cmath>
+#include <optional>
+
+template <typename T>
+struct Rectangle {
+    T x;
+    T y;
+    T width;
+    T height;
+
+    bool intersects(const Rectangle<T> &other) const
+    {
+        return !(x + width < other.x || x > other.x + other.width || y + height < other.y
+                 || y > other.y + other.height);
+    }
+
+    std::optional<Rectangle<T>> findIntersection(const Rectangle<T> &other) const
+    {
+        if (!intersects(other)) {
+            return std::nullopt;
+        }
+
+        T intersectX = std::max(x, other.x);
+        T intersectY = std::max(y, other.y);
+        T intersectWidth = std::min(x + width, other.x + other.width) - intersectX;
+        T intersectHeight = std::min(y + height, other.y + other.height) - intersectY;
+
+        return Rectangle<T>{intersectX, intersectY, intersectWidth, intersectHeight};
+    }
+};
