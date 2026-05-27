646ebad59cdef65402c41b26d4d35f0bea594f5b
51th08 <none@invalid.com>
2026-05-27 23:05:06 +0200

    Add compile LOG_STDOUT cmake option

--- src/game/systems/movement_system.cpp
+++ src/game/systems/movement_system.cpp
@@ -11,8 +11,9 @@ void MovementSystem::update(Registry &registry, float dt)
     for (auto entity : registry.view<Position, Velocity>()) {
         Position &position = registry.getComponent<Position>(entity);
         Velocity &velocity = registry.getComponent<Velocity>(entity);
-
+#ifdef LOG_STDOUT
         Position before = position;
+#endif
 
         position.x += velocity.dx * dt;
         position.y += velocity.dy * dt;
