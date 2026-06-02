aa2ffa73173315ec64731811b970770f2f1c4593
Stefan Rupert Walter Wagner <stefan.r.wagner@student.uibk.ac.at>
2026-06-02 12:15:16 +0200

    fix windows build

--- include/controller/persistence/serializer.hpp
+++ include/controller/persistence/serializer.hpp
@@ -28,8 +28,8 @@ class Serializer {
     {
         std::string json;
         if (const auto err = glz::write_json(value, json)) {
-            logger::log(logger::ERROR,
-                        std::format("Failed to serialize JSON for {}: {}", path.c_str(), glz::format_error(err, json)));
+            logger::log(logger::ERROR, std::format("Failed to serialize JSON for {}: {}", path.string(),
+                                                   glz::format_error(err, json)));
             return false;
         }
 
@@ -40,21 +40,21 @@ class Serializer {
 
             if (ec) {
                 logger::log(logger::ERROR,
-                            std::format("Failed to create directories for {}: {}", path.c_str(), ec.message()));
+                            std::format("Failed to create directories for {}: {}", path.string(), ec.message()));
                 return false;
             }
         }
 
         std::ofstream out(path);
         if (!out) {
-            logger::log(logger::ERROR, std::format("Failed to open file for writing: {}", path.c_str()));
+            logger::log(logger::ERROR, std::format("Failed to open file for writing: {}", path.string()));
             return false;
         }
 
         out << json;
 
         if (!out) {
-            logger::log(logger::ERROR, std::format("Failed to write JSON to file: {}", path.c_str()));
+            logger::log(logger::ERROR, std::format("Failed to write JSON to file: {}", path.string()));
             return false;
         }
         return true;
@@ -72,7 +72,7 @@ class Serializer {
         buffer << in.rdbuf();
 
         if (const auto err = glz::read_json(value, buffer.str())) {
-            logger::log(logger::ERROR, std::format("Failed to deserialize JSON for {}: {}", path.c_str(),
+            logger::log(logger::ERROR, std::format("Failed to deserialize JSON for {}: {}", path.string(),
                                                    glz::format_error(err, buffer.str())));
             return false;
         }
