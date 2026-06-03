66c7fa6bb9efa5c64fe1d882fbc92359e6ad2345
51th08 <none@invalid.com>
2026-06-03 10:43:56 +0200

    Refactor location_table.hpp

--- include/game/location_table.hpp
+++ include/game/location_table.hpp
@@ -9,7 +9,14 @@ namespace game {
 
 // Lookup 2D Table for finding entities based on their location in the world grid with dimensions view::gridWidth X
 // view::gridHeight.
-struct LocationTable {
+class LocationTable {
+  private:
+    std::vector<std::vector<Entity>> bucketGrid_;
+
+    std::vector<Entity> &getBucket(const unsigned buckIx, const unsigned buckIy);
+    std::tuple<Vec2<unsigned>, Vec2<unsigned>> getBucketIndices(Vec2<float> startGrid, Vec2<float> stopGrid);
+
+  public:
     const Vec2<unsigned> numBuckets;
     const Vec2<float> bucketSize;
 
@@ -18,20 +25,23 @@ struct LocationTable {
     std::unordered_set<Entity> getEntitiesNear(const Vec2<float> position, const float radius);
     std::vector<std::tuple<Entity, Position>> getEntitiesInRange(const Vec2<float> position, const float radius,
                                                                  const Registry &registry);
-    const std::vector<Entity> &cgetBucket(const unsigned buckIx, const unsigned buckIy) const
-    {
-        return bucketGrid_[buckIy * numBuckets.x + buckIx];
-    }
-    const std::vector<std::vector<Entity>> &cgetGrid() { return bucketGrid_; }
-
-  private:
-    std::vector<std::vector<Entity>> bucketGrid_;
-
-    std::vector<Entity> &getBucket(const unsigned buckIx, const unsigned buckIy)
-    {
-        return bucketGrid_[buckIy * numBuckets.x + buckIx];
-    }
-    std::tuple<Vec2<unsigned>, Vec2<unsigned>> getBucketIndices(Vec2<float> startGrid, Vec2<float> stopGrid);
+    const std::vector<Entity> &cgetBucket(const unsigned buckIx, const unsigned buckIy) const;
+    const std::vector<std::vector<Entity>> &cgetGrid();
 };
+
+inline const std::vector<Entity> &LocationTable::cgetBucket(const unsigned buckIx, const unsigned buckIy) const
+{
+    return bucketGrid_[buckIy * numBuckets.x + buckIx];
+}
+
+inline std::vector<Entity> &LocationTable::getBucket(const unsigned buckIx, const unsigned buckIy)
+{
+    return bucketGrid_[buckIy * numBuckets.x + buckIx];
+}
+
+inline const std::vector<std::vector<Entity>> &LocationTable::cgetGrid()
+{
+    return bucketGrid_;
+}
 
 } // namespace game
