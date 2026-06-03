3c0085a7328fe319d2c7dbdc8aeaa2c63578b55b
51th08 <none@invalid.com>
2026-06-03 10:35:14 +0200

    Refactor if() and .front()

--- src/game/systems/enemy_ai.cpp
+++ src/game/systems/enemy_ai.cpp
@@ -12,40 +12,40 @@ namespace game {
 void EnemyAI::update(Registry &registry, LocationTable &locationTable)
 {
     const auto players = registry.view<Velocity, PlayerStats>();
-    if (players.size() > 0) {
-        const auto player = players[0];
-        const Position playerPos = registry.getComponent<Position>(player);
-        const Vec2 posP{playerPos.x, playerPos.y};
+    if (players.empty()) {
+        return;
+    }
+    const auto player = players.front();
+    const Position playerPos = registry.getComponent<Position>(player);
+    const Vec2 posP{playerPos.x, playerPos.y};
 
-        for (auto enemy : registry.view<Velocity, view::Sprite, Position, EnemyTag, EnemyStats>()) {
-            // EnemyTag &enemyTag = registry.getComponent<EnemyTag>(enemy);
-            Velocity &velocity = registry.getComponent<Velocity>(enemy);
-            Position &enemyPos = registry.getComponent<Position>(enemy);
-            EnemyStats &enemyStats = registry.getComponent<EnemyStats>(enemy);
-            Vec2 posE{enemyPos.x, enemyPos.y};
+    for (auto enemy : registry.view<Velocity, view::Sprite, Position, EnemyTag, EnemyStats>()) {
+        // EnemyTag &enemyTag = registry.getComponent<EnemyTag>(enemy);
+        Velocity &velocity = registry.getComponent<Velocity>(enemy);
+        Position &enemyPos = registry.getComponent<Position>(enemy);
+        EnemyStats &enemyStats = registry.getComponent<EnemyStats>(enemy);
+        Vec2 posE{enemyPos.x, enemyPos.y};
 
-            // Set movement direction exactly towards player
-            Vec2 v = posP - posE;
-            auto playerDist = v.length();
+        // Set movement direction exactly towards player
+        Vec2 v = posP - posE;
 
-            // Calc. repelling force between enemies
-            auto enemiesInRange = locationTable.getEntitiesInRange(posE, 50, registry);
-            Vec2<float> repelOffset = {0, 0};
-            for (auto [e, position] : enemiesInRange) {
-                if (e == enemy) {
-                    continue;
-                }
-                const auto p = Vec2{position.x, position.y}; // TODO: into Vec2
-                const auto pToOther = (posE - p);
-                repelOffset += pToOther / (std::pow(pToOther.length(), 1.5)); // increase repelling with proximity
+        // Calc. repelling force between enemies
+        auto enemiesInRange = locationTable.getEntitiesInRange(posE, 50, registry);
+        Vec2<float> repelOffset = {0, 0};
+        for (auto [e, position] : enemiesInRange) {
+            if (e == enemy) {
+                continue;
             }
-            v.normalize();
-            v += repelOffset;
-
-            v.setLenght(enemyStats.moveSpeed);
-            velocity.dx = v.x; // TODO: into Vec2
-            velocity.dy = v.y; // TODO: into Vec2
+            const auto p = Vec2{position.x, position.y}; // TODO: into Vec2
+            const auto pToOther = (posE - p);
+            repelOffset += pToOther / (std::pow(pToOther.length(), 1.5)); // increase repelling with proximity
         }
+        v.normalize();
+        v += repelOffset;
+
+        v.setLenght(enemyStats.moveSpeed);
+        velocity.dx = v.x; // TODO: into Vec2
+        velocity.dy = v.y; // TODO: into Vec2
     }
 }
 
