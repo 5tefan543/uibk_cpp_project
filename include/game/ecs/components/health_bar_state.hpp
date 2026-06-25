aa9fd6fbd391de5035812173befd3f082eb74f97
Robert Zacchia <robert.zacchia@students.uibk.ac.at>
2026-06-25 15:38:06 +0200

    clang tidy

--- include/game/ecs/components/health_bar_state.hpp
+++ include/game/ecs/components/health_bar_state.hpp
@@ -7,7 +7,7 @@ struct HealthBarState {
     float initialRedBarNorm = 0.0f; // normalized red bar size at the moment damage was dealt (0..1)
     float redBarTimer = 0.0f;       // counts down from RED_FLASH_DURATION to 0
 
-    static constexpr float RED_FLASH_DURATION = 0.8f;
+    static constexpr float redFlashDuration = 0.8f;
 };
 
 } // namespace game
